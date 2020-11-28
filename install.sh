a327b8976234b05675abd56a76d57311e6a9cdc3
Sven Kõiv <ssven1995@gmail.com>
2020-11-29 00:41:57 +0200

    update installation script

--- install.sh
+++ install.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
-cp .vimrc ~/.vimrc
-cp .Xresources ~/.Xresources
-cp .bashrc ~/.bashrc
-cp .fzf.bash ~/.fzf.bash
-cp .i3 ~/.config/i3/config
-cp .i3blocks ~/.config/i3blocks/config
-cp -P .vim/after/ftplugin/* ~/.vim/after/ftplugin/
-cp .vim/colors/* ~/.vim/colors/
-cp .vim/indent/* ~/.vim/indent/
-cp .vim/coc-settings.json ~/.vim/
+read -p "WARNING: Your configuration files will be replaced. Continue? (y/n)" -n 1 -r
+echo
+
+
+replace() {
+    echo "Replacing $1 to: $HOME/$2"
+    cp -P "$1" "$HOME/$2"
+} 
+
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    replace .vimrc .vimrc
+    replace .Xresources .Xresources
+    replace .bashrc .bashrc
+    replace .fzf.bash .fzf.bash
+    replace .i3 .config/i3/config
+    replace .i3blocks .config/i3blocks/config
+    replace .vim/after/ftplugin/* .vim/after/ftplugin/
+    replace .vim/colors/* .vim/colors/
+    replace .vim/indent/* .vim/indent/
+    replace .vim/coc-settings.json .vim/
+fi
